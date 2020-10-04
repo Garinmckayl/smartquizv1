1ed0faefaadc8ffd346e1f62398c5ea6ae8d8f21
Sai Hegde <shegde@ancestry.com>
2020-10-04 11:57:35 -0600

    remove unwanted imports

--- src/main/java/com/anchormind/smartquiz/web/rest/QuizAttemptResource.java
+++ src/main/java/com/anchormind/smartquiz/web/rest/QuizAttemptResource.java
@@ -2,29 +2,33 @@ package com.anchormind.smartquiz.web.rest;
 
 import com.anchormind.smartquiz.service.QuizAttemptService;
 import com.anchormind.smartquiz.service.QuizService;
+import com.anchormind.smartquiz.service.dto.QuizAttemptDTO;
 import com.anchormind.smartquiz.service.dto.QuizDTO;
 import com.anchormind.smartquiz.web.rest.errors.BadRequestAlertException;
-import com.anchormind.smartquiz.service.dto.QuizAttemptDTO;
-
 import io.github.jhipster.web.util.HeaderUtil;
 import io.github.jhipster.web.util.PaginationUtil;
 import io.github.jhipster.web.util.ResponseUtil;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * REST controller for managing {@link com.anchormind.smartquiz.domain.QuizAttempt}.
